a837d03c4367d99eafcdc04ef0651b08f407dc1d
Vincent Sanders <vincesanders@hotmail.com>
2020-02-28 10:17:39 -0600

    Created the endpoints for the authRouter. Added jwtSecret to secrets. Created initial functionality for userModel. Created the generateToken utility.

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,39 @@
 const router = require('express').Router();
+const bcrypt = require('bcrypt');
+const users = require('../user/userModel');
+const generateToken = require('../utils/generateToken');
 
 router.post('/register', (req, res) => {
-  // implement registration
+    // implement registration
+    const { username, password } = req.body;
+    let user = { username, password };
+    const hash = bcrypt.hashSync(user.password, 8); //higher in production
+    user.password = hash;
+
+    users.insert(user).then(addedUser => {
+        const token = generateToken(addedUser);
+        res.status(201).json({ user: addedUser, token });
+    }).catch(({ error, message, stack }) => {
+        console.log(`Error: ${error}\nMessage: ${message}\n Stack: ${stack}`);
+        res.status(500).json({ error, message, stack, errorMessage: 'Unable to register user.' });
+    });
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+    // implement login
+    let { username, password } = req.body;
+
+    users.getBy({ username }).then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+            const token = generateToken(user);
+            res.status(200).json({ message: `Welcome back, ${username}!`, token});
+        } else {
+            res.status(401).json({ message: 'Invalid Credentials' });
+        }
+    }).catch(({error, message, stack}) => {
+        console.log(`Error: ${error}\nMessage: ${message}\n Stack: ${stack}`);
+        res.status(500).json({ error, message, stack, errorMessage: 'Unable to register user.' });
+    });
 });
 
 module.exports = router;
